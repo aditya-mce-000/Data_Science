6aa195880805e4a3ba6d1eb43332292501aa471d
cseaditya-000 <aditya3546raj@gmail.com>
2026-09-03 16:58:48 +0530

    -

--- Python_for_Everybody/Chapter8(Lists)/Sub_chap/03_parsing_and)extracting.py
+++
@@ -1,7 +0,0 @@
-fhand = open("C:\\Users\\adity\\OneDrive\\Documents(1)\\Programming\\Data_Science\\Python_for_Everybody\\mail_box.txt")
-for line in fhand:
-    if '@' in line:
-        data = line
-        found = data.find('@')
-        to_print = data.find(' ', found)
-        print(data[found+1:to_print])
